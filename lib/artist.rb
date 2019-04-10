6adfb6eac753b38e78bfc0bf8fb5103d3e6cfb36
Codey Pinero <codey.pinero@gmail.com>
2019-04-10 23:49:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,17 +1,3 @@
 class Artist
-  attr_accessor :name
-  @@songs = []
-  
-  def initialize(name)
-    @name = name
-  end
-  
-  def songs
-    @@songs
-  end
-  
-  def add_song
-    Song.new(track, self)
-  end
-  
+
 end
